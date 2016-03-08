d8e990b04bc2472aaf4e6b7a706dde9cff5a0109
Taardal <torbjorn.aardal@gmail.com>
2016-03-08 22:26:30 +0100

    Added constructor, getters and setters and toString-method for Person.

--- core/src/main/java/no/svitts/core/person/Person.java
+++ core/src/main/java/no/svitts/core/person/Person.java
@@ -3,8 +3,59 @@ package no.svitts.core.person;
 public class Person {
 
     private int id;
-    private int age;
     private String name;
+    private int age;
     private Gender gender;
     private Role role;
+
+    public Person(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", role=" + role +
+                '}';
+    }
 }
