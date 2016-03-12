c8bc9c42c6c03ab9f22e68508dc3cd865c38bb54
Taardal <torbjorn.aardal@gmail.com>
2016-03-12 15:31:12 +0100

    Added genres, genders and jobs

--- core/src/main/java/no/svitts/core/movie/Genre.java
+++ core/src/main/java/no/svitts/core/movie/Genre.java
@@ -4,7 +4,26 @@ public enum Genre {
 
     ACTION("Action"),
     ADVENTURE("Adventure"),
-    ANIMATION("Animation");
+    ANIMATION("Animation"),
+    BIOGRAPHY("Biography"),
+    COMEDY("Comedy"),
+    CRIME("Crime"),
+    DOCUMENTARY("Documentary"),
+    DRAMA("Drama"),
+    FAMILY("Family"),
+    FANTASY("Fantasy"),
+    FILM_NOIR("Film-Noir"),
+    HISTORY("History"),
+    HORROR("Horror"),
+    MUSIC("Music"),
+    MUSICAL("Musical"),
+    MYSTERY("Mystery"),
+    ROMANCE("Romance"),
+    SCI_FI("Sci-Fi"),
+    SPORT("Sport"),
+    THRILLER("Thriller"),
+    WAR("War"),
+    WESTERN("Western");
 
     private String value;
 
