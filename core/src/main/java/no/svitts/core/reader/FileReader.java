9336df0538a9aa91f887332748ef1b72b94c3bce
Taardal <torbjorn.aardal@gmail.com>
2016-03-19 19:21:19 +0100

    FileReader kan nå lese innholdet av resource-filer

--- core/src/main/java/no/svitts/core/reader/FileReader.java
+++ core/src/main/java/no/svitts/core/reader/FileReader.java
@@ -1,33 +1,45 @@
 package no.svitts.core.reader;
 
-import java.io.File;
-import java.io.IOException;
+import org.constretto.model.ClassPathResource;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class FileReader {
 
     private static final Logger LOGGER = Logger.getLogger(FileReader.class.getName());
 
+    public String readFile(String path) {
+        return readFile(new File(path));
+    }
+
     public String readFile(File file) {
         Path path = Paths.get(file.getPath());
-        return readFile(path);
-    }
-
-    public String readFile(String path) {
-        return readFile(Paths.get(path));
-    }
-
-    public String readFile(Path path) {
         try {
              return new String(Files.readAllBytes(path));
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Could not read file");
+            LOGGER.log(Level.SEVERE, "Could not read file", e);
         }
-        return "Could not read file";
+        return "";
+    }
+
+    public String readResource(String resourceName) {
+        return readResource(new ClassPathResource(resourceName));
+    }
+
+    public String readResource(ClassPathResource classPathResource) {
+        InputStream inputStream = classPathResource.getInputStream();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Could not read resource", e);
+        }
+        return "";
     }
 
 }
