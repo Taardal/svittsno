095e549fc553aaf82669b9c644e3b607e3ef265e
Taardal <torbjorn.aardal@gmail.com>
2016-04-23 00:45:38 +0200

    Refactoring

--- core/src/main/java/no/svitts/core/date/KeyDateParseException.java
+++
@@ -1,9 +0,0 @@
-package no.svitts.core.date;
-
-public class KeyDateParseException extends RuntimeException {
-
-    public KeyDateParseException(String message) {
-        super(message);
-    }
-
-}
