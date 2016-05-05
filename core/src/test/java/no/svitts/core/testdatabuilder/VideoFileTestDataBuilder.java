153a9d7512eceb5caffc9a8403959240f4142903
Taardal <torbjorn.aardal@gmail.com>
2016-05-05 17:46:05 +0200

    Added video file test data builder

---
+++ core/src/test/java/no/svitts/core/testdatabuilder/VideoFileTestDataBuilder.java
@@ -0,0 +1,62 @@
+package no.svitts.core.testdatabuilder;
+
+import no.svitts.core.file.VideoFile;
+import no.svitts.core.id.Id;
+
+import java.io.File;
+import java.io.IOException;
+
+public class VideoFileTestDataBuilder implements TestDataBuilder<VideoFile> {
+
+    private String id;
+    private String path;
+
+    public VideoFileTestDataBuilder() {
+        id = Id.get();
+        path = "path\\to\\videoFile";
+    }
+
+    @Override
+    public VideoFile build() {
+        return new VideoFile(id, path);
+    }
+
+    public VideoFileTestDataBuilder id(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public VideoFileTestDataBuilder path(String path) {
+        this.path = path;
+        return this;
+    }
+
+    public VideoFileTestDataBuilder tempFile() throws IOException {
+        path = File.createTempFile("tempFile", ".txt").getPath();
+        return this;
+    }
+
+    public VideoFileTestDataBuilder gerisGame() {
+        id = Id.get();
+        path = "\\\\TAARDAL-SERVER\\svitts\\geris game\\gerisgame.mkv";
+        return this;
+    }
+
+    public VideoFileTestDataBuilder forTheBirds() {
+        id = Id.get();
+        path = "\\\\TAARDAL-SERVER\\svitts\\for the birds\\forthebirds.mkv";
+        return this;
+    }
+
+    public VideoFileTestDataBuilder sherlockHolmes() {
+        id = Id.get();
+        path = "\\\\TAARDAL-SERVER\\svitts\\Sherlock Holmes 2009 1080p BluRay x264-SECTOR7\\s7-sherlock.holmes.1080p.x264.mkv";
+        return this;
+    }
+
+    public VideoFileTestDataBuilder sherlockHolmesAGameOfShadows() {
+        id = Id.get();
+        path = "\\\\TAARDAL-SERVER\\svitts\\Sherlock.Holmes.A.Game.Of.Shadows.2011.1080p.BluRay.x264-RSG [PublicHD]\\rsg-holmes2-1080p.mkv";
+        return this;
+    }
+}
