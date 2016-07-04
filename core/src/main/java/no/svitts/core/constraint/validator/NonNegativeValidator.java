5b605e0b8857e6715ac3cdafef3dd74dd44ba956
taardal <torbjorn.aardal@gmail.com>
2016-07-04 19:21:52 +0200

    Non negative number validator now supports long, double, float and integer

--- core/src/main/java/no/svitts/core/constraint/validator/NonNegativeValidator.java
+++ core/src/main/java/no/svitts/core/constraint/validator/NonNegativeValidator.java
@@ -5,7 +5,7 @@ import no.svitts.core.constraint.NonNegative;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class NonNegativeValidator extends CoreValidator implements ConstraintValidator<NonNegative, Integer> {
+public class NonNegativeValidator extends CoreValidator implements ConstraintValidator<NonNegative, Number> {
 
     @Override
     public void initialize(NonNegative constraintAnnotation) {
@@ -13,12 +13,25 @@ public class NonNegativeValidator extends CoreValidator implements ConstraintVal
     }
 
     @Override
-    public boolean isValid(Integer integer, ConstraintValidatorContext constraintValidatorContext) {
-        if (integer >= 0) {
+    public boolean isValid(Number number, ConstraintValidatorContext constraintValidatorContext) {
+        if (isNonNegative(number)) {
             return true;
         } else {
             addConstraintViolation("Number is below 0 (zero). Must be equal to or greater than 0 (zero).", constraintValidatorContext);
             return false;
         }
     }
+
+    private boolean isNonNegative(Number number) {
+        if (number instanceof Long) {
+            return (Long) number >= 0;
+        } else if (number instanceof Double) {
+            return (Double) number >= 0;
+        } else if (number instanceof Float) {
+            return (Float) number >= 0;
+        } else {
+            return (Integer) number >= 0;
+        }
+    }
+
 }
