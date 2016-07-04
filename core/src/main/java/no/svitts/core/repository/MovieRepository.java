1f8615eb33997bccb22a2ac018bc79754fcde1f5
taardal <torbjorn.aardal@gmail.com>
2016-07-04 18:15:36 +0200

    Added possibility to get multiple movies from database

--- core/src/main/java/no/svitts/core/repository/MovieRepository.java
+++ core/src/main/java/no/svitts/core/repository/MovieRepository.java
@@ -2,6 +2,7 @@ package no.svitts.core.repository;
 
 import com.google.inject.Inject;
 import no.svitts.core.criteria.Criteria;
+import no.svitts.core.criteria.CriteriaKey;
 import no.svitts.core.exception.RepositoryException;
 import no.svitts.core.movie.Movie;
 import org.hibernate.HibernateException;
@@ -9,6 +10,9 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class MovieRepository extends CoreRepository<Movie> {
@@ -33,15 +37,22 @@ public class MovieRepository extends CoreRepository<Movie> {
 
     @Override
     public List<Movie> getMultiple(Criteria criteria) {
-        return null;
+        LOGGER.info("Getting multiple movies from database by criteria [{}].", criteria.toString());
+        try {
+            CriteriaBuilder criteriaBuilder = getCurrentSession().getCriteriaBuilder();
+            CriteriaQuery<Movie> movieCriteriaQuery = getMovieCriteriaQuery(criteria, criteriaBuilder);
+            return getCurrentSession().createQuery(movieCriteriaQuery).setMaxResults(criteria.getLimit()).setFirstResult(criteria.getOffset()).getResultList();
+        } catch (HibernateException | IllegalStateException e) {
+            LOGGER.error("Could not get multiple movies from database by criteria [{}]", criteria.toString());
+            throw new RepositoryException(e);
+        }
     }
 
     @Override
     public String saveSingle(Movie movie) {
         LOGGER.info("Inserting movie [{}] into database.", movie.toString());
         try {
-            getCurrentSession().save(movie);
-            return movie.getId();
+            return (String) getCurrentSession().save(movie);
         } catch (HibernateException e) {
             LOGGER.error("Could not save movie [{}] to database.", movie.toString(), e);
             throw new RepositoryException(e);
@@ -49,8 +60,27 @@ public class MovieRepository extends CoreRepository<Movie> {
     }
 
     @Override
-    public void deleteSingle(String id) {
-        getCurrentSession().delete(getSingle(id));
+    public void deleteSingle(Movie movie) {
+        LOGGER.info("Deleting movie [{}] from database.", movie.toString());
+        try {
+            getCurrentSession().delete(movie);
+        } catch (HibernateException e) {
+            LOGGER.error("Could not delete movie [{}] from database.", movie.toString(), e);
+            throw new RepositoryException(e);
+        }
+    }
+
+    private CriteriaQuery<Movie> getMovieCriteriaQuery(Criteria criteria, CriteriaBuilder criteriaBuilder) {
+        CriteriaQuery<Movie> movieCriteriaQuery = criteriaBuilder.createQuery(Movie.class);
+        Root<Movie> movieRoot = movieCriteriaQuery.from(Movie.class);
+        movieCriteriaQuery.select(movieRoot);
+        if (criteria.get(CriteriaKey.NAME) != null) {
+            movieCriteriaQuery.where(criteriaBuilder.like(movieRoot.get("name"), criteria.get(CriteriaKey.NAME)));
+        }
+        if (criteria.get(CriteriaKey.GENRE) != null) {
+            movieCriteriaQuery.having(criteriaBuilder.like(movieRoot.get("genre"), criteria.get(CriteriaKey.GENRE)));
+        }
+        return movieCriteriaQuery;
     }
 
 }
--- core/src/test/java/no/svitts/core/repository/MovieRepositoryTest.java
+++ core/src/test/java/no/svitts/core/repository/MovieRepositoryTest.java
@@ -1,39 +1,51 @@
 package no.svitts.core.repository;
 
 import no.svitts.core.builder.MovieBuilder;
+import no.svitts.core.criteria.Criteria;
+import no.svitts.core.criteria.CriteriaKey;
 import no.svitts.core.exception.RepositoryException;
 import no.svitts.core.movie.Movie;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
-import java.sql.SQLException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 import static no.svitts.core.testkit.MovieTestKit.assertMovie;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class MovieRepositoryTest {
 
-    private SessionFactory sessionFactoryMock;
-    private Session sessionMock;
     private MovieRepository movieRepository;
     private MovieBuilder movieBuilder;
 
-    @Before
-    public void setUp() throws SQLException {
-        sessionFactoryMock = mock(SessionFactory.class);
-        sessionMock = mock(Session.class);
-        when(sessionFactoryMock.getCurrentSession()).thenReturn(sessionMock);
+    @Mock
+    private SessionFactory sessionFactoryMock;
 
+    @Mock
+    private Session sessionMock;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        when(sessionFactoryMock.getCurrentSession()).thenReturn(sessionMock);
         movieRepository = new MovieRepository(sessionFactoryMock);
         movieBuilder = new MovieBuilder();
     }
 
     @Test
-    public void getOne_ValidId_ShouldReturnMovie() throws Exception {
+    public void getSingle_ValidId_ShouldReturnMovie() {
         Movie movie = movieBuilder.build();
         when(sessionMock.get(Movie.class, movie.getId())).thenReturn(movie);
 
@@ -45,14 +57,45 @@ public class MovieRepositoryTest {
     }
 
     @Test(expected = RepositoryException.class)
-    public void getOne_ThrowsHibernateException_ShouldCatchHibernateExceptionAndThrowRepositoryException() throws Exception {
+    public void getSingle_ThrowsHibernateException_ShouldCatchHibernateExceptionAndThrowRepositoryException() {
         Movie movie = movieBuilder.build();
-        when(sessionMock.get(Movie.class, movie.getId())).thenThrow(new HibernateException("Expected"));
+        when(sessionMock.get(Movie.class, movie.getId())).thenThrow(new HibernateException("Exception"));
         movieRepository.getSingle(movie.getId());
     }
 
     @Test
-    public void saveOne_ValidMovie_ShouldReturnIdOfSavedMovie() throws Exception {
+    public void getMultiple_ValidCriteria_ShouldReturnMovies() {
+        List<Movie> movies = getMovies();
+        Criteria criteria = getCriteria();
+
+        CriteriaBuilder criteriaBuilderMock = mock(CriteriaBuilder.class);
+        when(sessionMock.getCriteriaBuilder()).thenReturn(criteriaBuilderMock);
+
+        CriteriaQuery criteriaQueryMock = mock(CriteriaQuery.class);
+        when(criteriaBuilderMock.createQuery(Movie.class)).thenReturn(criteriaQueryMock);
+
+        Root rootMock = mock(Root.class);
+        when(criteriaQueryMock.from(Movie.class)).thenReturn(rootMock);
+
+        Query queryMock = mock(Query.class);
+        when(sessionMock.createQuery(criteriaQueryMock)).thenReturn(queryMock);
+        when(queryMock.setMaxResults(criteria.getLimit())).thenReturn(queryMock);
+        when(queryMock.setFirstResult(criteria.getOffset())).thenReturn(queryMock);
+        when(queryMock.getResultList()).thenReturn(movies);
+
+        List<Movie> moviesFromRepository = movieRepository.getMultiple(criteria);
+
+        assertArrayEquals(movies.toArray(), moviesFromRepository.toArray());
+        verify(sessionFactoryMock, times(2)).getCurrentSession();
+        verify(sessionMock, times(1)).getCriteriaBuilder();
+        verify(sessionMock, times(1)).createQuery(criteriaQueryMock);
+        verify(queryMock, times(1)).setMaxResults(criteria.getLimit());
+        verify(queryMock, times(1)).setFirstResult(criteria.getOffset());
+        verify(queryMock, times(1)).getResultList();
+    }
+
+    @Test
+    public void saveSingle_ValidMovie_ShouldReturnIdOfSavedMovie() {
         Movie movie = movieBuilder.build();
         when(sessionMock.save(movie)).thenReturn("");
 
@@ -64,11 +107,40 @@ public class MovieRepositoryTest {
     }
 
     @Test(expected = RepositoryException.class)
-    public void saveOne_ThrowsHibernateException_ShouldCatchHibernateExceptionAndThrowRepositoryException() throws Exception {
+    public void saveSingle_ThrowsHibernateException_ShouldCatchHibernateExceptionAndThrowRepositoryException() {
         Movie movie = movieBuilder.build();
-        when(sessionMock.save(movie)).thenThrow(new HibernateException("Expected"));
+        when(sessionMock.save(movie)).thenThrow(new HibernateException("Exception"));
         movieRepository.saveSingle(movie);
     }
 
+    @Test
+    public void deleteSingle_ValidMovie_ShouldDeleteMovie() {
+        Movie movie = movieBuilder.build();
+        doNothing().when(sessionMock).delete(movie);
+        verify(sessionMock, times(1)).delete(movie);
+    }
+
+    @Test(expected = RepositoryException.class)
+    public void deleteSingle_ThrowsHibernateException_ShouldCatchHibernateExceptionAndThrowRepositoryException() {
+        Movie movie = movieBuilder.build();
+        doThrow(new HibernateException("Exception")).when(sessionMock).delete(movie);
+        movieRepository.deleteSingle(movie);
+    }
+
+    private List<Movie> getMovies() {
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movieBuilder.name("movie1").build());
+        movies.add(movieBuilder.name("movie2").build());
+        return movies;
+    }
+
+    private Criteria getCriteria() {
+        Criteria criteria = new Criteria();
+        criteria.setLimit(50);
+        criteria.setOffset(10);
+        criteria.add(CriteriaKey.NAME, "name");
+        criteria.add(CriteriaKey.GENRE, "genre");
+        return criteria;
+    }
 
 }
