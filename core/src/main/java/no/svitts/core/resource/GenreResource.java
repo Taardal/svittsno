173fcccd9f0090abfba9fae9cffb8c4127083c22
Taardal <torbjorn.aardal@gmail.com>
2016-08-12 21:53:00 +0200

    Refactoring

--- core/src/main/java/no/svitts/core/resource/GenreResource.java
+++ core/src/main/java/no/svitts/core/resource/GenreResource.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import no.svitts.core.genre.Genre;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,13 +11,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("genres")
-@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Api(value = "Genre resource", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+@Api(value = "Genre resource", produces = MediaType.APPLICATION_JSON)
 public class GenreResource {
 
     @GET
-    @ApiOperation(value = "Get all genres.")
+    @ApiOperation(value = "Get all genres")
     public Response getGenres() {
         return Response.ok(Genre.values()).build();
     }
