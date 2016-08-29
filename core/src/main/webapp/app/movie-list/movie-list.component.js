5dc9da6febe0187164b5985c816420bf167a7c01
taardal <torbjorn.aardal@gmail.com>
2016-08-29 17:14:36 +0200

    Movie list component can now play movie

--- core/src/main/webapp/app/movie-list/movie-list.component.js
+++ core/src/main/webapp/app/movie-list/movie-list.component.js
@@ -1,17 +1,30 @@
 'use-strict';
 
 angular.module('movieList').component('movieList', {
-    // templateUrl: 'app/movie-list/movie-list.template.html',
-    template: 'TBD: Detail view for <span>{{$ctrl.genre}}</span>',
-    controller: ['$routeParams', function ($routeParams) {
+    templateUrl: 'app/movie-list/movie-list.template.html',
+    controller: function ($http, $routeParams, notificationService) {
 
         var ctrl = this;
         ctrl.genre = $routeParams.genre;
 
         $http.get('./api/v1/movies/genres/' + ctrl.genre).then(function (response) {
             ctrl.movies = response.data;
-        })
+        });
+
+        ctrl.playMovie = function (movie) {
+            var request = {
+                method: 'GET',
+                url: 'http://localhost:8181/player',
+                headers: {
+                    path: movie.videoFile.path
+                }
+            };
+            $http(request).then(function () {
+                notificationService.success("Could not play movie. Is the player running?");
+            }, function () {
+                notificationService.error("Could not play movie. Is the player running?");
+            });
+        }
 
     }
-    ]
 });
